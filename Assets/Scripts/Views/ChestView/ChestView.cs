4148927b7f1ef4077df5012c483af56bdeeda5c0
Ujj Ádám <adamujj66@mailbox.unideb.hu>
2026-02-26 18:51:50 +0100

    Click me eltüntetés fix

--- Assets/Scripts/Views/ChestView/ChestView.cs
+++ Assets/Scripts/Views/ChestView/ChestView.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class ChestView : MonoBehaviour
 {
     [SerializeField] private GameObject chestMenuUI;
+    [SerializeField] private GameObject clickmeText;
 
     private void OnMouseDown()
     {
         chestMenuUI.SetActive(true);
+        clickmeText.SetActive(false);
     }
 }
--- Assets/Scripts/Views/HealView/HealStationView.cs
+++ Assets/Scripts/Views/HealView/HealStationView.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class HealStationView : MonoBehaviour
 {
     [SerializeField] private GameObject healMenuUI;
+    [SerializeField] private GameObject clickmeText;
 
     private void OnMouseDown()
     {
         healMenuUI.SetActive(true);
+        clickmeText.SetActive(false);
     }
 }
