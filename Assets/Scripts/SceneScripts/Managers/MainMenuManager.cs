49545f467826e4682acc982e9389a43308f13d54
Ujj Ádám <adamujj66@mailbox.unideb.hu>
2026-03-07 10:51:24 +0100

    Tutorial hozzáadása a játékhoz és ahhoz tartozó fixek

--- Assets/Scripts/SceneScripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/SceneScripts/Managers/MainMenuManager.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject Tutorialpage1;
+    [SerializeField] private GameObject Tutorialpage2;
+    [SerializeField] private GameObject Tutorialpage3;
+    [SerializeField] private GameObject Tutorialpage4;
+    [SerializeField] private GameObject Tutorialplane;
     public void SessionStart()
     {
         SceneController.Instance
@@ -17,5 +22,32 @@ public class MainMenuManager : MonoBehaviour
     public void Kilepes()
     {
         Application.Quit();
+    }
+
+    public void BeginTutorial()
+    {
+        Tutorialplane.SetActive(true);
+    }
+
+    public void Next1()
+    {
+        Tutorialpage1.SetActive(false);
+        Tutorialpage2.SetActive(true);
+    }
+    public void Next2()
+    {
+        Tutorialpage2.SetActive(false);
+        Tutorialpage3.SetActive(true);
+    }
+    public void Next3()
+    {
+        Tutorialpage3.SetActive(false);
+        Tutorialpage4.SetActive(true);
+    }
+    public void ExitTutorial()
+    {
+        Tutorialplane.SetActive(false);
+        Tutorialpage4.SetActive(false);
+        Tutorialpage1.SetActive(true);
     }
 }
